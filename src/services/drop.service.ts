9613ead807e265e0ab3aa827942894516e0e58b2
Pyroarsonist <velikiydan@gmail.com>
2020-05-28 22:11:49 +0300

    oops

--- src/services/drop.service.ts
+++ src/services/drop.service.ts
@@ -5,7 +5,7 @@ import { ViewerData } from '../repositories';
 @Injectable()
 export class DropService {
   public async pickWinner(dropId: number): Promise<ViewerData> {
-    const viewer = await ViewerData.findOne({);
+    const viewer = await ViewerData.findOne();
     // const viewer = await ViewerData.findOne({ order: Sequelize.fn('RANDOM()') });
     console.log({ viewer });
     return viewer;
